991a23ba12705e028bb122452b45af5fd3a1e264
Raidel Martin-Barrios <rmartin9301@gmail.com>
2025-04-30 11:46:31 +0200

    Add install_deepmd.sh to scripts directory

---
+++ scripts/install_deepmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Load Conda into this shell
+source ~/anaconda3/etc/profile.d/conda.sh
+
+echo "Creating environment ml-md-env3..."
+cd ..
+conda env create -f environment.yml
+
+# Activate the conda environment (adjust name if needed)
+echo "Activating environment ml-md-env3..."
+conda activate ml-md-env || { echo "Failed to activate the environment"; exit 1; }
+
+
+# Ensure system compiler is used instead of Anaconda's internal one
+#echo "Switching to system compiler..."
+#export PATH=/usr/bin:$PATH
+
+# Install DeepMD-kit from source
+echo "Installing DeepMD-kit from source..."
+pip install --no-binary deepmd-kit deepmd-kit
+
+echo "✅ DeepMD-kit installation completed successfully."
